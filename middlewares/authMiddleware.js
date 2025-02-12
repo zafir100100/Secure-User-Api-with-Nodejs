e413042bb0010803aa276df739b01d75c69088ab
Zafir <55599023+zafir100100@users.noreply.github.com>
2025-02-12 21:24:07 +0600

    authetication middleware improved

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,19 +1,34 @@
 const jwt = require('jsonwebtoken');
 
+class AuthMiddleware {
+    constructor(secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    authenticateToken = (req, res, next) => {
+        const authHeader = req.header('Authorization');
+
+        if (!authHeader) {
+            return res.status(403).send('Access denied');
+        }
+
+        const [scheme, token] = authHeader.split(' ');
+
+        if (scheme !== 'Bearer' || !token) {
+            return res.status(400).send('Invalid token format');
+        }
+
+        jwt.verify(token, this.secretKey, (err, user) => {
+            if (err) {
+                return res.status(403).send('Invalid token');
+            }
+            req.user = user;
+            next();
+        });
+    }
+}
+
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
+const authMiddleware = new AuthMiddleware(SECRET_KEY);
 
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.header('Authorization');
-    if (!authHeader) return res.status(403).send('Access denied');
-
-    const [scheme, token] = authHeader.split(' ');
-    if (scheme !== 'Bearer' || !token) return res.status(400).send('Invalid token format');
-
-    jwt.verify(token, SECRET_KEY, (err, user) => {
-        if (err) return res.status(403).send('Invalid token');
-        req.user = user;
-        next();
-    });
-};
-
-module.exports = { authenticateToken };
+module.exports = { authenticateToken: authMiddleware.authenticateToken };
